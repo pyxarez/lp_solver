6b97960700be9127f3e26b789ba3a405cd0b8200
Alexander <sanyk_strelok@mail.ru>
2017-01-24 14:24:34 +0400

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+let gulp = require('gulp'),
+sass = require('gulp-sass'),
+browserSync = require('browser-sync');
+
+gulp.task('sass', () => { 
+	return gulp.src('app/scss/**/*.scss')
+	.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+	.pipe(gulp.dest('app/css'))
+	.pipe(browserSync.reload({stream: true}));		
+});
+
+gulp.task('browser-sync', () => {
+	browserSync({
+		server: {
+			baseDir: 'app'
+		},
+		port: 8080,
+		tunnel: "sanchez",
+		notify: false
+	});
+});
+
+gulp.task('watch', ["browser-sync", "sass"], () => {
+	gulp.watch('app/scss/**/*.scss', ['sass']);
+	gulp.watch('app/*.html', browserSync.reload);
+	gulp.watch('app/js/**/*.js', browserSync.reload);
+});
+
+gulp.task('default', ['watch']);
