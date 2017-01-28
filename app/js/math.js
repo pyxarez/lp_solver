f701c7679f2e1ee64a53b4abec0e9cb0237848f5
Alexander <sanyk_strelok@mail.ru>
2017-01-28 16:18:38 +0400

    переместил целевую функцию в математическую часть, добавил функции подсчёта в математическую часть

--- app/js/math.js
+++ app/js/math.js
@@ -1,6 +1,42 @@
 // Массив ограничений (уравнений системы ограничения)
 let equations = [];
 
+/* 
+	* Массив значений целевой функции 
+*/
+let valuesOfTargetFunction = [];
+
+/* 
+  * Массив пересечений, для отрисовки
+*/
+let bounds = [];
+
+/* 
+  * Целевая функция
+*/
+let targetFunction = {
+  x1: null,
+  x2: null,
+
+  /**
+   * Метод для подсчёта значения целевой функции
+   *
+   * @param {number} х1 значение х1.
+   * @param {number} х2 значение х2.
+   * @return {number} значение целевой функции при данных х1 и х2.
+   */
+
+  calculate: function(x1, x2) {
+    return this.x1 * x1 + this.x2 * x2; 
+  },
+
+  init: function(x1, x2) {
+    this.x1 = x1;
+    this.x2 = x2;
+  }
+};
+
+
 class Equation {
   constructor(x1, x2, sign, value) {
     this.x1 = x1;
@@ -23,10 +59,6 @@ class Graph {
   }
 }
 
-/* 
-	* Массив значений целевой функции 
-*/
-let valuesOfTargetFunction = [];
 
 /*  Вычисляем систему уравнений */
 
@@ -48,8 +80,7 @@ let valuesOfTargetFunction = [];
  * Вычисляет значение х1 для состемы уравнений на основе х2
  *
  * @param {object} con1 первое уравнение(ограничение).
- * @param {object} con2 второе уравнение(ограничение).
- * @param {number} значение переменной х2 для системы уравнений.
+ * @param {number} x1 значение переменной х2 для системы уравнений.
  * @return {number} x1 значение переменной х1.
  */
  function computeX1(con1, x2) {
@@ -61,15 +92,41 @@ let valuesOfTargetFunction = [];
 /**
  * Вычилсяет х2 при известном х1, нужно для просчёта пересечений с осью х2(вертикальной) 
  *
- * @param {} .
- * @param {} .
- * @param {} .
- * @return {number} .
+ * @param {object} con1 первое уравнение(ограничение).
+ * @param {number} x1 значение переменной х1 для системы уравнений.
+ * @return {number} x2 значение переменной х2.
  */
 
 function computeX2ByX1(con1, x1) {
-  let x2 =  
+  let x2 =  (con1.value - con1.x1 * x1) / con1.x2;
+
+  return x2;
+}
+
+/**
+ * Функция проверки принадлежности точки к ОДР
+ *
+ * @param {object} con ограничение(выражение).
+ * @param {number} x1 значение х1 для ограничения.
+ * @param {number} х2 значение х2 для ограничение.
+ * @return {boolean} возращается результат проверки на принадлежность к одр для ограниченния.
+ */
+
+function checkAccessory(con, x1, x2) {
+  let computedValue = con.x1 * x1 + con.x2 * x2;
+
+  if (computedValue <= con.value) return true;
+
+  return false;
 }
 
 
-/*  Тестоввые */
+/*  Тестовая инициализация  */
+
+equations.push(new Equation(3, 4, "<=", 1700));
+equations.push(new Equation(12, 30, "<=", 9600));
+
+targetFunction.init(2, 4);
+
+
+
